d6cded72781fa2be37b66b4772f95b7a0dc5821e
yassine <yassineaghla23@gmail.com>
2025-05-17 13:54:14 +0100

    migration pour les articles

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -4,14 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Document</title>
 </head>
 <body>
     <div>
-    <a href="{{route('categories.create')}}">creer un nouvelle categorie</a>
+    <a href="{{route('categories.create')}}"class="btn btn-primary">creer un nouvelle categorie</a>
     </div>
-    <div>    
-    <table>
+    <div class="container">    
+    <table class="table">
         <tr>
         <th>Id</th>
         <th>name</th>
@@ -22,8 +23,8 @@
           <td>{{$item->id}}</td>
           <td>{{$item->name}}</td>
           <td>
-            <a href="{{route('categories.edit',$item)}}">modifier</a>
-            <form action="{{route('categories.destroy',$item)}}" method="POST">
+            <a href="{{route('categories.edit',$item)}}" class="btn btn-warning">modifier</a>
+            <form action="{{route('categories.destroy',$item)}}" method="POST" class="btn btn-danger">
                 @csrf
                 @method('DELETE')
 
